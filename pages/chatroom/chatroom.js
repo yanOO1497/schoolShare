d1ab1c100c209297d4e923e8a39c2961b416c48b
yanOO1497 <yuanyuan1497@foxmail.com>
2018-04-24 20:33:28 +0800

    feat:聊天室

--- pages/chatroom/chatroom.js
+++ pages/chatroom/chatroom.js
@@ -6,14 +6,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userData:{
-      userInfo: app.globalData.userInfo,
+    userData: {
       userId: "id",
-      nickname:"名字",
-      avaterUrl:"url",
-      chatData:{}
+      nickname: "名字",
+      avaterUrl: "avater-holder.jpg"
     },
-    
+    chatData: [{"0":"这里是你发的消息"},
+      { "1": "这里是我发的消息" },
+      { "0": "这里是你发的消息" },
+      { "0": "这里是你发的消息" },
+      { "0": "这里是你发的消息" },
+      { "1": "这里是我发的消息" },
+      { "0": "这里是你发的消息" },
+      { "0": "这里是你发的消息" },
+      { "0": "这里是你发的消息" },
+      { "1": "这里是我发的消息" },
+      { "0": "这里是你发的消息" },
+      { "0": "这里是你发的消息" },  
+    ],
+    myAvaterUrl: ''
 
   },
 
@@ -23,60 +34,62 @@ Page({
   onLoad: function (options) {
     var that = this;
     that.setData({
-      userData:options
+      userData: options,
+      myAvaterUrl: app.globalData.userInfo.avatarUrl
     })
+    
     wx.setNavigationBarTitle({
       title: options.nickname//页面标题为路由参数
     })
-    console.log(app);
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
