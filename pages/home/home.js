407434b5e6f6be4a7931ab299861daa093cdd8f9
yanOO1497 <yuanyuan1497@foxmail.com>
2018-04-30 16:49:50 +0800

    feat:添加有赞UI框架，readme更新

--- pages/home/home.js
+++ pages/home/home.js
@@ -14,14 +14,41 @@ Page({
     //搜索框
     inputShowed: false,
     inputVal: "",
-    tabs: ["广场", "问答精华", "分享精华","师哥师姐说","校园推广活动"],
     activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0,
     //最新消息列表
     listData: [],
     listQua:[],
-    listShare:[]
+    listShare:[],
+    navtab: {
+      list: [{
+        id: 'quiz',
+        title: '校内问答'
+      }, {
+        id: 'share',
+        title: '经验分享'
+      }, {
+          id: 'lectures',
+        title: '讲座'
+      }, {
+          id: 'activity',
+        title: '社团/团立项活动'
+      }, {
+        id: 'reward',
+        title: '悬赏求助'
+      }, {
+          id: ' preferential',
+        title: '商家优惠'
+      }, {
+          id: 'employ',
+        title: '招聘日历'
+      }
+      ],
+      selectedId: 'share',
+      scroll: true,
+      height: 45
+    }
   },//data
   /**
    * 生命周期函数--监听页面加载
@@ -35,16 +62,17 @@ Page({
         avatarUrl: "http://p3.music.126.net/NgCeNHxZcuqCsJTo_tdRKA==/18637821604170727.jpg?param=30y30",
         coverUrl: "http://img1.gtimg.com/tj/pics/hv1/151/175/2020/131395276.jpg",
         title: "四六级又没过？这是我最后一次抢救你了（碾压四六级全套经验）",
-        desc: "英语四六级和普通的日常英语不一样，毕竟是考试，要求的是笔试和听力，并没有口语，所以提升起来也可以相对的快一些。所以呢，我觉得只要有一点点的英语基础，加上自己的重视，过4，6级其实并不难。",
+        content: "英语四六级和普通的日常英语不一样，毕竟是考试，要求的是笔试和听力，并没有口语，所以提升起来也可以相对的快一些。所以呢，我觉得只要有一点点的英语基础，加上自己的重视，过4，6级其实并不难。",
         comment: "33",
         love: "3",
-        uid: "11"
+        uid: "11",
+        
       }, {
         type: "wenda",
         nickName: "holy俊辉",
         avatarUrl: "https://pic.qqtn.com/up/2018-4/15241053731750196.jpg",
         title: "标题标题标题标题标题",
-        // desc: "摘要摘要",
+        content: "摘要摘要",
         comment: "33",
         love: "3",
         uid: "22",
@@ -54,7 +82,7 @@ Page({
         nickName: "怀左同学",
         avatarUrl: "https://pic.qqtn.com/up/2018-4/15241053732525556.jpg",
         title: "有钱之前，先让自己值钱",
-        desc: "刚到北京的那几天，朋友正处于失业期，我们挑了一个时间，在紫阳公园聊了一下午。 工作后的人，通常比还在学校的同龄人成长要快，一个人",
+        content: "刚到北京的那几天，朋友正处于失业期，我们挑了一个时间，在紫阳公园聊了一下午。 工作后的人，通常比还在学校的同龄人成长要快，一个人",
         comment: "33",
         love: "3",
         uid: "33"
@@ -63,7 +91,7 @@ Page({
         nickName: "媛媛",
         avatarUrl: "http://p3.music.126.net/NgCeNHxZcuqCsJTo_tdRKA==/18637821604170727.jpg?param=30y30",
         title: "四六级又没过？这是我最后一次抢救你了（碾压四六级全套经验）",
-        desc: "英语四六级和普通的日常英语不一样，毕竟是考试，要求的是笔试和听力，并没有口语，所以提升起来也可以相对的快一些。所以呢，我觉得只要有一点点的英语基础，加上自己的重视，过4，6级其实并不难。",
+        content: "英语四六级和普通的日常英语不一样，毕竟是考试，要求的是笔试和听力，并没有口语，所以提升起来也可以相对的快一些。所以呢，我觉得只要有一点点的英语基础，加上自己的重视，过4，6级其实并不难。",
         comment: "33",
         love: "3",
         uid: "11"
@@ -80,7 +108,7 @@ Page({
         nickName: "怀左同学",
         avatarUrl: "https://pic.qqtn.com/up/2018-4/15241053732525556.jpg",
         title: "有钱之前，先让自己值钱",
-        desc: "刚到北京的那几天，朋友正处于失业期，我们挑了一个时间，在紫阳公园聊了一下午。 工作后的人，通常比还在学校的同龄人成长要快，一个人",
+        content: "刚到北京的那几天，朋友正处于失业期，我们挑了一个时间，在紫阳公园聊了一下午。 工作后的人，通常比还在学校的同龄人成长要快，一个人",
         comment: "33",
         love: "3",
         uid: "33"
@@ -90,7 +118,7 @@ Page({
         nickName: "holy俊辉",
         avatarUrl: "https://pic.qqtn.com/up/2018-4/15241053731750196.jpg",
         title: "福大东门怎么走？",
-        // desc: "摘要摘要",
+        // content: "摘要摘要",
         comment: "4",
         love: "3",
         uid: "22",
@@ -100,7 +128,7 @@ Page({
         nickName: "萌新萌新",
         avatarUrl: "https://pic.qqtn.com/up/2018-4/15241053731750196.jpg",
         title: "谁有高数下的历年卷？",
-        // desc: "摘要摘要",
+        // content: "摘要摘要",
         comment: "33",
         love: "3",
         uid: "23",
@@ -111,7 +139,7 @@ Page({
         nickName: "媛媛",
         avatarUrl: "http://p3.music.126.net/NgCeNHxZcuqCsJTo_tdRKA==/18637821604170727.jpg?param=30y30",
         title: "四六级又没过？这是我最后一次抢救你了（碾压四六级全套经验）",
-        desc: "英语四六级和普通的日常英语不一样，毕竟是考试，要求的是笔试和听力，并没有口语，所以提升起来也可以相对的快一些。所以呢，我觉得只要有一点点的英语基础，加上自己的重视，过4，6级其实并不难。",
+        content: "英语四六级和普通的日常英语不一样，毕竟是考试，要求的是笔试和听力，并没有口语，所以提升起来也可以相对的快一些。所以呢，我觉得只要有一点点的英语基础，加上自己的重视，过4，6级其实并不难。",
         comment: "33",
         love: "3",
         uid: "11"
@@ -175,28 +203,7 @@ Page({
   onShareAppMessage: function () {
 
   },
-  //搜索框
-  showInput: function () {
-    this.setData({
-      inputShowed: true
-    });
-  },
-  hideInput: function () {
-    this.setData({
-      inputVal: "",
-      inputShowed: false
-    });
-  },
-  clearInput: function () {
-    this.setData({
-      inputVal: ""
-    });
-  },
-  inputTyping: function (e) {
-    this.setData({
-      inputVal: e.detail.value
-    });
-  },
+ 
   //nav点击事件
   tabClick: function (e) {
     this.setData({
